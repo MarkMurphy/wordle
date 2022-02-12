c50f31d4a4d34a29b1d1dcb5f76501cbb03d02f3
Mark Murphy <MarkMurphy@users.noreply.github.com>
2022-02-12 19:40:46 -0400

    Refactor modals

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 import dictionary from "./scripts/dictionary.js";
 import words from "./scripts/words.js";
 
+const DANCE_ANIMATION_DURATION = 500;
+const FLIP_ANIMATION_DURATION = 500;
+const MODAL_CLASS_NAME_OPEN = "open";
+const STATE_ACTIVE = "active";
+const STATE_ABSENT = "absent";
+const STATE_PRESENT = "present";
+const STATE_CORRECT = "correct";
+
+const keyboard = document.querySelector("[data-keyboard]");
+const grid = document.querySelector("[data-letter-grid]");
+const alerts = document.querySelector("[data-alert-container]");
+
+const WORD_LENGTH = 5;
+const START_DATE = new Date(2022, 0, 1);
+const wordle = Math.floor((Date.now() - START_DATE) / 1000 / 60 / 60 / 24);
+const word = words[wordle % words.length];
+
 const root = document.documentElement;
 const theme = window.matchMedia?.("(prefers-color-scheme: light)") || {};
 
 changeTheme(theme);
+startInteraction();
 
 function changeTheme({ matches }) {
   root.dataset.theme = matches ? "light" : "dark";
@@ -12,35 +30,11 @@ function changeTheme({ matches }) {
 
 theme.addEventListener("change", changeTheme);
 
-startInteraction();
-
-const WORD_LENGTH = 5;
-const FLIP_ANIMATION_DURATION = 500;
-const DANCE_ANIMATION_DURATION = 500;
-
-const STATE_ACTIVE = "active";
-const STATE_ABSENT = "absent";
-const STATE_PRESENT = "present";
-const STATE_CORRECT = "correct";
-
-const helpButton = document.querySelector("[data-help-button]");
-const statisticsButton = document.querySelector("[data-statistics-button]");
-const settingsButton = document.querySelector("[data-settings-button]");
-const shareButton = document.querySelector("[data-share-button]");
-
-const keyboard = document.querySelector("[data-keyboard]");
-const grid = document.querySelector("[data-letter-grid]");
-const alerts = document.querySelector("[data-alert-container]");
-
-const START_DATE = new Date(2022, 0, 1);
-const wordle = Math.floor((Date.now() - START_DATE) / 1000 / 60 / 60 / 24);
-const word = words[wordle % words.length];
-
 const settings = {
   disableAbsentLetters: false,
 };
 
-document.addEventListener("change", (e) => {
+const handleSettingChange = (event) => {
   const setting = e.target.dataset.setting;
 
   if (setting == null) {
@@ -50,12 +44,19 @@ document.addEventListener("change", (e) => {
   if (setting === "disableAbsentLetters") {
     settings.disableAbsentLetters = Boolean(e.target.checked);
   }
-});
+};
 
-helpButton.addEventListener("click", openHelp);
-statisticsButton.addEventListener("click", openStatistics);
-settingsButton.addEventListener("click", openSettings);
+document.addEventListener("change", handleSettingChange);
 
+const handleModalButton = (e) => {
+  if (e.target.matches('[data-toggle="modal"]')) {
+    openModal(e.target.dataset.target);
+  }
+};
+
+document.addEventListener("click", handleModalButton);
+
+const shareButton = document.querySelector("[data-share-button]");
 shareButton.addEventListener("click", share);
 
 async function share() {
@@ -142,71 +143,16 @@ function handleMouseClick(e) {
   }
 }
 
-function openModal(name) {
-  const modal = document.querySelector(`[data-modal="${name}"]`);
-  if (modal == null) {
-    return;
-  }
-
-  // close any currently open modals
-  document.querySelectorAll(`[data-modal].open`).forEach(closeModal);
-
-  const close = modal.querySelector("[data-close]");
-  const overlay = modal.querySelector("[data-overlay]");
-  const handleModalClose = (e) => {
-    if (e instanceof KeyboardEvent && e.key === "Escape") {
-      close.removeEventListener("clink", handleModalClose);
-      closeModal(name);
-    }
-
-    if (e instanceof MouseEvent) {
-      overlay.removeEventListener("click", handleModalClose);
-      document.removeEventListener("keydown", handleModalClose);
-      closeModal(name);
-    }
-  };
-
-  close.addEventListener("click", handleModalClose, { once: true });
-  overlay.addEventListener("click", handleModalClose, { once: true });
-  document.addEventListener("keydown", handleModalClose, { once: true });
-
-  modal.classList.add("open");
-}
-
-function closeModal(modal) {
-  if (typeof modal === "string") {
-    modal = document.querySelector(`[data-modal="${modal}"]`);
-  }
-
-  if (modal == null) {
-    return;
-  }
-
-  modal.classList.remove("open");
-  modal.classList.add("closing");
-  modal.addEventListener(
-    "animationend",
-    () => {
-      modal.classList.remove("closing");
-    },
-    { once: true }
-  );
+function openHelp() {
+  openModal("#instructions");
 }
 
 function openSettings() {
-  openModal("settings");
+  openModal("#settings");
 }
 
 function openStatistics() {
-  openModal("statistics");
-}
-
-function openHelp() {
-  openModal("help");
-}
-
-function openStatistics() {
-  openModal("statistics");
+  openModal("#statistics");
 }
 
 function handleKeyDown(e) {
@@ -346,6 +292,58 @@ function showAlert(message, duration = 1000) {
   }, duration);
 }
 
+function openModal(selector) {
+  const selector = e.target.dataset.target;
+  const modal = document.querySelector(selector);
+  const backdrop = modal.querySelector("[data-modal-backdrop]");
+  const body = modal.querySelector("[data-modal-body]");
+  const dismiss = modal.querySelector('[data-dismiss="modal"]');
+
+  modal.style.display = "block";
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", true);
+  modal.setAttribute("role", "dialog");
+  modal.scrollTop = 0;
+
+  if (body) {
+    body.scrollTop = 0;
+  }
+
+  reflow(modal);
+
+  modal.classList.add(MODAL_CLASS_NAME_OPEN);
+
+  const close = () => {
+    backdrop.removeEventListener("click", handleDismiss);
+    dismiss?.removeEventListener("click", handleDismiss);
+    document.removeEventListener("keydown", handleEscape);
+    const handleTransitonEnd = () => {
+      modal.style.display = "none";
+      modal.setAttribute("aria-hidden", true);
+      modal.removeAttribute("aria-modal");
+      modal.removeAttribute("role");
+    };
+    modal.addEventListener("transitionend", handleTransitonEnd, {
+      once: true,
+    });
+    modal.classList.remove(MODAL_CLASS_NAME_OPEN);
+  };
+
+  const handleEscape = (event) => {
+    if (event.key === "Escape") {
+      close();
+    }
+  };
+
+  const handleDismiss = (event) => {
+    close();
+  };
+
+  backdrop.addEventListener("click", handleDismiss, { once: true });
+  dismiss?.addEventListener("click", handleDismiss, { once: true });
+  document.addEventListener("keydown", handleEscape);
+}
+
 function shakeTiles(tiles) {
   tiles.forEach((tile) => {
     tile.classList.add("shake");
@@ -392,3 +390,15 @@ function checkWinLose(guess, tiles) {
     showStats();
   }
 }
+
+/**
+ * Trick to restart an element's animation
+ *
+ * @param {HTMLElement} element
+ * @return void
+ *
+ * @see https://www.charistheo.io/blog/2021/02/restart-a-css-animation-with-javascript/#restarting-a-css-animation
+ */
+export const reflow = (element) => {
+  element.offsetHeight; // eslint-disable-line no-unused-expressions
+};
